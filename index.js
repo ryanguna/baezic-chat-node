f407f1496b8bf476284032319ec83fa27a5c8f7a
Ryan Gunawardana <ryan.g@triuneelectronics.com>
2018-04-18 13:36:24 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const app = require('express')(),
+    socketIOServer = require("http").createServer(app),
+    io = require("socket.io")(socketIOServer);
+
+
+let messages = [];
+
+let users = {};
+
+io.on('connection', function(socket){
+
+
+    socket.on('setUsername', function(data, fn) {
+
+        if(users[socket.id]){
+            fn(false);
+        }else{
+            fn(true);
+
+            users[socket.id] = {
+                nickname : data.nickname,
+                connection_id : socket.id
+            };
+            let message = {
+                action : true,
+                connection_id : socket.id,
+                nickname : data.nickname,
+                message : data.nickname + ' joined the room'
+            };
+            messages.push(message);
+            io.sockets.emit('updatePublicMessages', message);
+
+            io.sockets.connected[socket.id].emit('updateCurrentUser', { nickname : data.nickname, connection_id : socket.id });
+
+            io.sockets.emit('listOfUsers',Object.keys(users).map(key => users[key]));
+        }
+
+    });
+
+    socket.on('loadAllMessages', function(data, fn) {
+        io.sockets.emit('allMessages', messages);
+    });
+
+
+    socket.on('sendUserMessage', function(data, fn) {
+        let message = {
+            action : false,
+            connection_id : users[socket.id].id,
+            nickname : users[socket.id].nickname,
+            message : data
+        };
+        messages.push(message);
+        io.sockets.emit('updatePublicMessages', message);
+
+        fn(true);
+    });
+
+
+    // socket.on('loadmessage', function(data, fn) {
+    //     console.log('MESSAGE LOAD DAW');
+    //     io.sockets.emit('broadcast', messages);
+    // });
+
+    // socket.on('connecting', function(reas) {
+    //     console.log('received to pare q connect', data);
+    // });
+
+
+    socket.on('disconnecting', (reason) => {
+
+        if(users[socket.id]){
+            let message = {
+                action : true,
+                connection_id : users[socket.id].connection_id,
+                nickname : users[socket.id].nickname,
+                message : users[socket.id].nickname + ' left the room'
+            };
+            messages.push(message);
+            delete users[socket.id];
+            io.sockets.emit('updatePublicMessages', message);
+            io.sockets.emit('listOfUsers',Object.keys(users).map(key => users[key]));
+        }
+
+
+    });
+
+
+
+
+});
+
+socketIOServer.listen(8081);
