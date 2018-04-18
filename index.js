d7913517c413bf84a790d069bfea4ea02af98c60
Ryan Gunawardana <ryan.g@triuneelectronics.com>
2018-04-18 14:06:00 +0800

    added files

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const app = require('express')(),
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+        .use(express.static(path.join(__dirname, 'public')))
+        .set('views', path.join(__dirname, 'views'))
+        .set('view engine', 'ejs')
+        .get('/', (req, res) => res.render('pages/index'))
+        .listen(PORT, () => console.log(`Listening on ${ PORT }`)),
     socketIOServer = require("http").createServer(app),
     io = require("socket.io")(socketIOServer);
 
